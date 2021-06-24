aeb0aa08a5b7d629b3ba40dadbbffbdafa0ff4ac
Byeongjin Hwang <taddy0818@naver.com>
2021-06-24 22:31:16 +0900

    퍼즐게임 - 타일 섞기

--- IMAGE-PUZZLE/main.js
+++ IMAGE-PUZZLE/main.js
@@ -9,9 +9,30 @@ const tileCount = 16;
 //     const $li = document.createElement('li');
 //     $container.append($li);
 // }
-Array(tileCount).fill().forEach((v, i) => {
-    const $li = document.createElement('li');
-    $li.setAttribute('data-index', i);
-    $li.classList.add(`list${i}`);
-    $container.appendChild($li);
-});
+
+let tiles = [];
+tiles = createImageTiles();
+shuffle(tiles).forEach(tile=>$container.appendChild(tile)) // 인자가 하나인경우 중괄호 생략가능
+
+function createImageTiles() {
+    const tempArray = [];
+    Array(tileCount).fill().forEach((v, i) => {
+        const $li = document.createElement('li');
+        $li.setAttribute('data-index', i);
+        $li.classList.add(`list${i}`);
+        tempArray.push($li);
+        // $container.appendChild($li);
+    });
+    return tempArray;
+}
+
+function shuffle(array){
+    let index = array.length - 1;
+    while(index > 0) {
+        const randomIndex = Math.floor(Math.random()*(index+1));
+        [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+        index--;
+    }
+    return array;
+
+}
